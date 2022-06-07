02ba34f153532c471ab963ca2ff8777905d65123
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2022-06-07 21:29:52 +0100

    added a create ranger api call

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 
+const pool = require('./db');
+
 const port = process.env.PORT || 3000;
+
+app.use(express.json()); // => req.body
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded( {extended: true}));
@@ -16,6 +20,23 @@ app.get('/', (req, res) => {
 app.get('/index', (req, res) => {
     res.sendFile(path.join(__dirname,'public','index.html'));
 });
+
+
+//create a new ranger
+app.post("/createRanger", async(req, res) => {
+    try {
+        const { rangerName } = req.body;
+        const { username } = req.body;
+        const { password } = req.body;
+    
+        const newAdmin = await pool.query("INSERT INTO ranger (rangerName, username, AdminPassword) VALUES ($1, $2, $3)", [rangerName, username, password]);
+    
+        res.json("Ranger successfully created");
+    } catch (err) {
+        console.error(err.message);
+        res.json("username already exists");
+    }
+    });
 
 const loginRouter = require('./routes/loginRouter');
 const monitorRouter = require('./routes/monitorRouter');
